a13ac561c827547e213d1f7052c85949edea7774
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-03-17 10:37:11 +0700

    init backend

--- backend/go.mod
+++ backend/go.mod
@@ -1,9 +1,5 @@
-module github.com/pongsakorn-maker/react-phone-store/backend
+module github.com/pongsakorn-maker/react-phone-store
 
-go 1.15
+go 1.16
 
-require (
-	github.com/gofiber/cors v0.2.2 // indirect
-	github.com/gofiber/fiber v1.14.6
-	github.com/jinzhu/gorm v1.9.16
-)
+require github.com/gofiber/fiber v1.14.6
--- backend/schema/phone.go
+++ backend/schema/phone.go
@@ -1,51 +1,25 @@
-package book
+package phone
 
 import (
 	"github.com/gofiber/fiber"
-	"github.com/jinzhu/gorm"
-	"github.com/pongsakorn-maker/react-phone-store/backend/database"
 )
 
-type Phone struct {
-	gorm.Model
-	Phone string `json:"Phone"`
-}
-
 func GetPhones(c *fiber.Ctx) {
-	db := database.DBConnection
-	var phones []Phone
-	db.Find(&phones)
-	c.JSON(phones)
+	c.Status(200).Send("Get all phones")
 }
 
 func GetPhone(c *fiber.Ctx) {
-	id := c.Params("id")
-	db := database.DBConnection
-	var phone Phone
-	db.Find(&phone, id)
-	c.JSON(phone)
+	c.Status(200).Send("Get a phone")
 }
 
 func CreatePhone(c *fiber.Ctx) {
-	db := database.DBConnection
-	phone := new(Phone)
-	if err := c.BodyParser(phone); err != nil {
-		c.Status(503).Send(err)
-		return
-	}
-	db.Create(&phone)
-	c.JSON(phone)
+	c.Status(201).Send("Create a new phone")
+}
+
+func UpdatePhone(c *fiber.Ctx) {
+	c.Status(200).Send("Update a phone")
 }
 
 func DeletePhone(c *fiber.Ctx) {
-	id := c.Params("id")
-	db := database.DBConnection
-	var phone Phone
-	db.First(&phone, id)
-	if phone.Phone == "" {
-		c.Status(500).Send("No phone found with given ID")
-		return
-	}
-	db.Delete(&phone)
-	c.Send("phone successfully deleted")
+	c.Status(204).Send("Delete a phone")
 }
